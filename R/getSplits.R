4962fcb750393700fd0e6dce40df096774556712
Anna Vesely <anna.vesely92@gmail.com>
2022-10-13 11:28:50 +0200

    fixed getSplit (getting signs was useless)

--- R/getSplits.R
+++ R/getSplits.R
@@ -1,6 +1,6 @@
 #' @title Data Splits and Variable Selectiong
 #' @description Internal function. It splits the data into two subsets of equal size,
-#' then uses the first subset to select variables and estimate coefficients.
+#' then uses the first subset to select variables.
 #' @usage getSplits(X, Y, Q, target, varSel, varSelArgs, seed)
 #' @param X numeric design matrix (excluding the intercept), where columns correspond to variables, and rows to observations.
 #' @param Y numeric response vector.
@@ -16,11 +16,11 @@
 #' \itemize{
 #' \item \code{obs}: list of the \code{Q} index vectors corresponding to observations not used for variable selection
 #' \item \code{vars}: list of the \code{Q} index vectors corresponding to selected variables
-#' \item \code{signs}: vector containing, for each variable, the mode of the estimated coefficients' signs
 #' }
 #' @author Anna Vesely.
 #' @noRd
 
+#\item \code{signs}: vector containing, for each variable, the mode of the estimated coefficients' signs
 
 getSplits <- function(X, Y, Q, target, varSel, varSelArgs, seed){
 
@@ -33,23 +33,23 @@ getSplits <- function(X, Y, Q, target, varSel, varSelArgs, seed){
 
   obs <- vector(mode="list", length=Q)
   vars <- vector(mode="list", length=Q)
-  signs <- matrix(0, ncol=m, nrow=Q)
-  # signs <- rep(0,m) # mode of the estimated coefficients
+  # signs <- matrix(0, ncol=m, nrow=Q)
 
   for(q in seq(Q)){
     # randomly split the data in 2 subsets
     D1 <- sample(seq(n), n0) # first subset
     V <- sort(do.call(varSel, args=c(list(X=X[D1,], Y=Y[D1], target=target), varSelArgs))) # selected variables
-    coeff <- lm(Y[D1] ~ X[D1,V])$coefficients[-1] # coefficients of selected vars
 
     obs[[q]] <- setdiff(seq(n), D1)
     vars[[q]] <- V
-    signs[q,V] <- 2 * (sign(coeff) >= 0) - 1
-    # signs[V] <- signs[V] + sign(coeff)
+
+    # if(length(V) > 0){
+    #   coeff <- lm(Y[D1] ~ X[D1,V])$coefficients[-1] # coefficients of selected vars
+    #   signs[q,V] <- 2 * (sign(coeff) >= 0) - 1
+    # }
   }
 
-  # signs <- 2*(signs >= 0) - 1 # mode of coefficient's signs
-
-  out <- list("obs"=obs, "vars"=vars, "signs"=signs)
+  # out <- list("obs"=obs, "vars"=vars, "signs"=signs)
+  out <- list("obs"=obs, "vars"=vars)
   return(out)
 }
